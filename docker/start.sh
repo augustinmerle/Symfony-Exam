6724a07e5aa45cc23e2b835410c563966444d9f4
augustin merle <augustin@laveaucoupet.fr>
2017-03-10 10:24:28 +0100

    add command for starting up

--- docker/start.sh
+++ docker/start.sh
@@ -9,6 +9,12 @@ if [ "$SYMFONY_ENV" = 'dev' ]; then
 else
     composer install --prefer-dist --no-dev --no-progress --no-suggest --optimize-autoloader --classmap-authoritative
 fi
+# Create Database an mmodel
+bin/console doctrine:database:create
+bin/console doctrine:schema:update --force
+
+# Import ciqual from url
+bin/console bin/console fmu:ciqual-importer
 
 # Start Apache with the right permissions after removing pre-existing PID file
 rm -f /var/run/apache2/apache2.pid
